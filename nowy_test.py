61fc275db5a13e670bff660ccbca8750ccb950a4
PienkowskiAdam <106634880+PienkowskiAdam@users.noreply.github.com>
2024-01-08 11:08:15 +0100

    Update nowy_test.py

--- nowy_test.py
+++ nowy_test.py
@@ -1,5 +1,6 @@
 import os
-import fitz  # PyMuPDF
+from PyPDF2 import PdfFileReader
+from pdf2image import convert_from_path
 
 def find_pdf_file(directory):
     for root, dirs, files in os.walk(directory):
@@ -9,18 +10,12 @@ def find_pdf_file(directory):
     return None
 
 def extract_images_from_pdf(pdf_path, output_folder):
-    pdf_document = fitz.open(pdf_path)
-    
-    for page_num in range(pdf_document.page_count):
-        page = pdf_document[page_num]
-        image_list = page.get_images(full=True)
-        
-        for img_index, img_info in enumerate(image_list):
-            img_index += 1
-            img = page.get_pixmap(img_info[0])
-            img.save(os.path.join(output_folder, f"page_{page_num + 1}_image_{img_index}.png"))
-
-    pdf_document.close()
+    with open(pdf_path, 'rb') as file:
+        pdf_reader = PdfFileReader(file)
+        for page_num in range(pdf_reader.numPages):
+            images = convert_from_path(pdf_path, first_page=page_num + 1, last_page=page_num + 1)
+            for i, image in enumerate(images):
+                image.save(os.path.join(output_folder, f"page_{page_num + 1}_image_{i + 1}.png"), "PNG")
 
 if __name__ == "__main__":
     pdf_directory = r"C:\Users\dzemo\Desktop\Python\konwerter pit"
